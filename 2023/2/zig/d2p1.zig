9aa48062438584b5b286202014f17a1403ec8284
Mason Schmidgall <mason.schmidgall1@gmail.com>
2023-12-02 01:21:15 -0600

    changes to 2023 d2p1

--- 2023/2/zig/d2p1.zig
+++ 2023/2/zig/d2p1.zig
@@ -10,10 +10,23 @@ const Game = struct {
     pullList: []Pull,
     id: u32,
 
-    pub fn init(allocator: anytype) Game {
+    pub fn init(allocator: std.mem.Allocator) Game {
+        var pullList = std.ArrayList(Pull).init(allocator);
+
         return Game {
-            
-        }
+            .id = undefined,
+            .pullList = pullList,
+        };
+    }
+
+    pub fn deinit(self: Game) void {
+        self.pullList.deinit();
+    }
+
+    pub fn parseGame(reader: anytype, allocator: std.mem.Allocator) !Game {
+        var game = Game.init(allocator);
+        var buffer: [32]u8 = undefined;
+        const input = try reader.readUntilDelimiterOrEof()
     }
 };
 
@@ -36,6 +49,9 @@ fn parseGame(reader: anytype) !Game {
 }
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
     const stdin = std.io.getStdIn();
     const stdout = std.io.getStdOut();
 
