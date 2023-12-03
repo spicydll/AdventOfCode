94e1536731c8e6f02048a6a1d367638c9e11e75c
Mason Schmidgall <mason.schmidgall1@gmail.com>
2023-12-03 00:30:46 -0600

    It compiles but it don't work :(

--- 2023/2/zig/d2p1.zig
+++ 2023/2/zig/d2p1.zig
@@ -1,32 +1,25 @@
 const std = @import("std");
 
-const Pull = struct {
-    red: u32,
-    green: u32,
-    blue: u32
-};
+const Pull = struct { red: u32, green: u32, blue: u32 };
 
-const Color = enum {
-    red, green, blue
-};
+const Color = enum { red, green, blue };
 
 // very much a hack!!
 fn whichColor(buf: []const u8) ?Color {
-    
     return switch (buf.len) {
         3 => Color.red,
         4 => Color.blue,
         5 => Color.green,
-        else => null
+        else => null,
     };
 }
 
 const Game = struct {
-    pullList: []Pull,
+    pullList: std.ArrayList(Pull),
     id: u32,
 
     pub fn init(allocator: std.mem.Allocator) Game {
-        return Game {
+        return Game{
             .id = undefined,
             .pullList = std.ArrayList(Pull).init(allocator),
         };
@@ -36,7 +29,7 @@ const Game = struct {
         self.pullList.deinit();
     }
 
-    pub fn parseGame(reader: anytype, allocator: std.mem.Allocator) !Game {
+    pub fn parseGame(reader: anytype, allocator: std.mem.Allocator) !?Game {
         var game = Game.init(allocator);
         var buffer: [32]u8 = undefined;
 
@@ -44,7 +37,7 @@ const Game = struct {
         const idInput = try reader.readUntilDelimiter(&buffer, ':');
         var idSplitter = std.mem.split(u8, idInput, " ");
         _ = idSplitter.next().?; // Don't need first half
-        const column_half = idSplitter.next() orelse return error.ParseError;
+        const column_half = idSplitter.next() orelse return null;
         game.id = try std.fmt.parseUnsigned(u32, column_half, 10);
 
         // Read in pullList
@@ -62,15 +55,22 @@ const Game = struct {
                 const col1 = valueSplitter.next() orelse return error.ParseError;
                 const col2 = valueSplitter.next() orelse return error.ParseError;
 
-                const num: u32 = std.fmt.parseUnsigned(u32, col1, 10) orelse return error.ParseError;
+                const num = try std.fmt.parseUnsigned(u32, col1, 10);
+
                 const color = whichColor(col2).?;
                 switch (color) {
-                    Color.red => {pull.red = num;},
-                    Color.green => {pull.green = num;},
-                    Color.blue => {pull.blue = num;},
+                    Color.red => {
+                        pull.red = num;
+                    },
+                    Color.green => {
+                        pull.green = num;
+                    },
+                    Color.blue => {
+                        pull.blue = num;
+                    },
                 }
 
-                game.pullList.append(pull) orelse return error.OutOfMemoryError;
+                try game.pullList.append(pull);
             }
         }
 
@@ -84,9 +84,12 @@ pub fn main() !void {
     const allocator = arena.allocator();
     const stdin = std.io.getStdIn();
     const stdout = std.io.getStdOut();
-
+    var gameList = std.ArrayList(Game).init(allocator);
 
     while (true) {
-
+        const game = (try Game.parseGame(stdin.reader(), allocator)) orelse break;
+        try gameList.append(game);
     }
+
+    try stdout.writer().print("Len: {}", .{gameList.items.len});
 }
