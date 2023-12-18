4c985b5e134d41ccd85b9fabbda265b1175ea3b1
Mason Schmidgall <mason.schmidgall@madren.org>
2023-12-18 17:27:58 -0600

    Nearly there with day12p1

--- 2023/12/zig/d12p1.zig
+++ 2023/12/zig/d12p1.zig
@@ -18,9 +18,8 @@ const SpringState = enum {
 const SpringRow = struct {
     springs: []const SpringState,
     damaged: []const u64,
-    operational: []u64,
 
-    pub fn parseSpringRows(reader: std.io.AnyReader, allocator: std.mem.Allocator) ![]const SpringRow {
+    pub fn parseSpringRows(reader: anytype, allocator: std.mem.Allocator) ![]const SpringRow {
         var spring_row_list = std.ArrayList(SpringRow).init(allocator);
         defer spring_row_list.deinit();
 
@@ -43,7 +42,7 @@ const SpringRow = struct {
             }
             const springs = try spring_list.toOwnedSlice();
 
-            var damaged_list = std.ArrayList(u32).init(allocator);
+            var damaged_list = std.ArrayList(u64).init(allocator);
             var damage_splitter = std.mem.splitScalar(u8, damaged_part, ',');
             var damage_sum: u64 = 0;
             while (damage_splitter.next()) |damage| {
@@ -52,17 +51,9 @@ const SpringRow = struct {
                 try damaged_list.append(damage_num);
             }
             const damaged = try damaged_list.toOwnedSlice();
-            var operational: []u64 = try allocator.alloc(u64, damaged.len + 1);
-            for (operational, 0..) |_, i| {
-                operational[i] = 1;
-            }
-            operational[0] = 0;
-            operational[operational.len - 1] = @as(u64, @intCast(springs.len - (operational.len - 2))) - damage_sum;
-
             const spring_row: SpringRow = .{
                 .springs = springs,
                 .damaged = damaged,
-                .operational = operational,
             };
 
             try spring_row_list.append(spring_row);
@@ -71,87 +62,99 @@ const SpringRow = struct {
         return try spring_row_list.toOwnedSlice();
     }
 
-    fn checkCurrentPermutation(self: SpringRow) bool {
-        var spring_i: usize = 0;
-        for (self.operational, 0..) |operational, i| {
-            const operational_size: usize = @as(usize, @intCast(operational));
-
-            const spring_i_start = spring_i;
-            while ((spring_i - spring_i_start) < operational_size) : (spring_i += 1) {
-                const valid = switch (self.springs[spring_i]) {
-                    SpringState.operational => true,
-                    SpringState.unknown => true,
-                    SpringState.damaged => false,
-                };
-
-                if (!valid) {
-                    return false;
-                }
-            }
-
-            if (i < self.damaged.len) {
-                const damage_size: usize = @as(usize, @intCast(self.damaged[i]));
-                const spring_i_next = spring_i;
-                while ((spring_i - spring_i_next) < damage_size) : (spring_i += 1) {
-                    const valid = switch (self.springs[spring_i]) {
-                        SpringState.operational => false,
-                        SpringState.unknown => true,
-                        SpringState.damaged => true,
-                    };
-
-                    if (!valid) {
-                        return false;
-                    }
-                }
-            }
+    fn getNumDamaged(self: SpringRow) u64 {
+        var num_damaged: u64 = 0;
+        for (self.damaged) |num| {
+            num_damaged += num;
         }
-
-        return true;
+        return num_damaged;
     }
 
-    fn inflate(self: *SpringRow, part: usize) !void {
-        const take_from: usize = switch (part) {
-            0 => self.operational.len - 1,
-            else => part - 1,
-        };
-
-        const before_take = self.operational[take_from] - 1;
-
-        if (before_take == 1) {
-            if (take_from != 0 and take_from != self.operational.len - 1) {
-                return error.CannotInflate;
+    fn countPossibilities(self: SpringRow, start_spring: usize, first_group: usize) u64 {
+        const impossible: u64 = 0;
+        const single_possibility: u64 = 1;
+        if (start_spring >= self.springs.len) {
+            if (first_group >= self.damaged.len) {
+                return single_possibility;
             }
-        } else if (before_take == 0) {
-            return error.CannotInflate;
+            return impossible;
         }
 
-        self.operational[take_from] -= 1;
-        self.operational[part] += 1;
-    }
+        const springs = self.springs[start_spring..];
 
-    fn permuteToSum(sum: u64, num: usize, min: u64, allocator: std.mem.Allocator) std.ArrayList([]const u64) {
-        _ = min;
-        _ = sum;
-        var numbers: [num]u64 = undefined;
-        _ = numbers;
-        var perm_list = std.ArrayList([]u64).init(allocator);
-        _ = perm_list;
-
-        
-    }
-
-    pub fn checkPermutations(self: *SpringRow) u64 {
-        var num_permutations: u64 = 0;
-
-        for (self.operational, 0..) |_, i| {
-            while (true) {
-                if (self.checkCurrentPermutation()) {
-                    num_permutations += 1;
+        if (first_group >= self.damaged.len) {
+            for (springs) |spring| {
+                if (spring == SpringState.damaged) {
+                    return impossible;
                 }
-                self.inflate(i) catch break;
             }
+            return single_possibility;
         }
+
+        if (start_spring > 0 and self.springs[start_spring - 1] == SpringState.damaged) {
+            return impossible;
+        }
+
+        const groups = self.damaged[first_group..];
+
+        if (@as(usize, @intCast(groups[0])) > springs.len) {
+            return impossible;
+        }
+
+        if (springs[0] == SpringState.operational) {
+            return self.countPossibilities(start_spring + 1, first_group);
+        }
+        var num_damaged: u64 = 1;
+        var pure = true;
+        for (springs[1..]) |spring| {
+            if (spring == SpringState.operational) {
+                break;
+            }
+            if (spring == SpringState.unknown) {
+                pure = false;
+            }
+            num_damaged += 1;
+        }
+
+        if (num_damaged < groups[0]) {
+            return impossible;
+        }
+
+        if (num_damaged == groups[0]) {
+            // One possible spot for this group
+            return self.countPossibilities(start_spring + @as(usize, @intCast(num_damaged)) + 1, first_group + 1);
+        }
+
+        if (pure) {
+            // too big
+            return impossible;
+        }
+
+        var total_possibilities = self.countPossibilities(start_spring + @as(usize, @intCast(groups[0])) + 1, first_group + 1);
+        total_possibilities += self.countPossibilities(start_spring + 1, first_group);
+        std.debug.print("Returning: {d}\n", .{total_possibilities});
+        return total_possibilities;
+    }
+
+    pub fn checkPermutations(self: SpringRow) u64 {
+        return self.countPossibilities(0, 0);
     }
 };
 
-pub fn main() !void {}
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const stdin = std.io.getStdIn();
+
+    const spring_rows = try SpringRow.parseSpringRows(stdin.reader(), allocator);
+
+    var sum: u64 = 0;
+    for (spring_rows) |spring_row| {
+        const perms = spring_row.checkPermutations();
+        std.debug.print("{d}\n", .{perms});
+        sum += perms;
+    }
+
+    try std.io.getStdOut().writer().print("Sum: {d}\n", .{sum});
+}
