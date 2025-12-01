d095ceb98d47c6e24f9c17436961e705439b7acc
Mason Schmidgall <mason.schmidgall1@gmail.com>
2025-12-01 13:29:29 -0600

    finish d1p2

--- 2025/d1/d1p2.zig
+++ 2025/d1/d1p2.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Reader = std.io.Reader;
+const Writer = std.io.Writer;
 
 fn parseLine(r: *Reader) !?i32 {
     const line = try r.takeDelimiter('\n') orelse return null;
@@ -16,42 +17,96 @@ fn parseLine(r: *Reader) !?i32 {
     };
 }
 
-pub fn main() !void {
+fn moveCountZero(pos: *i32, move: i32) u32 {
+    const super_pos = pos.* + move;
+    var pass: u32 = @abs(@divFloor(super_pos, 100));
+    const new_pos: i32 = @mod(super_pos, 100);
+
+    if (move < 0) {
+        if (pos.* == 0) {
+            pass -|= 1;
+        }
+        if (new_pos == 0) {
+            pass += 1;
+        }
+    }
+
+    pos.* = new_pos;
+    return pass;
+}
+
+test moveCountZero {
+    var pos: i32 = 50;
+    const pos_ptr = &pos;
+
+    const TestCase = struct {
+        move: i32,
+        pos: i32,
+        pass: u32,
+    };
+
+    const test_cases = [_]TestCase{
+        .{ .move = -68, .pos = 82, .pass = 1 },
+        .{ .move = -30, .pos = 52, .pass = 0 },
+        .{ .move = 48, .pos = 0, .pass = 1 },
+        .{ .move = -5, .pos = 95, .pass = 0 },
+        .{ .move = 60, .pos = 55, .pass = 1 },
+        .{ .move = -55, .pos = 0, .pass = 1 },
+        .{ .move = -1, .pos = 99, .pass = 0 },
+        .{ .move = -99, .pos = 0, .pass = 1 },
+        .{ .move = 14, .pos = 14, .pass = 0 },
+        .{ .move = -82, .pos = 32, .pass = 1 },
+    };
+
+    for (test_cases) |case| {
+        try std.testing.expectEqual(case.pass, moveCountZero(pos_ptr, case.move));
+        try std.testing.expectEqual(case.pos, pos);
+    }
+}
+
+fn getPass(r: *Reader, w: *Writer) !u32 {
     var pos: i32 = 50;
     var pass: u32 = 0;
 
-    while (try parseLine(stdin)) |move| {
-        try stdout.print("{d} -> ", .{pos});
-        const dist_to_0: i32 = if (move < 0)
-            pos
-        else
-            100 - pos;
+    while (try parseLine(r)) |move| {
+        try w.print("{d} -> ", .{pos});
+        pass += moveCountZero(&pos, move);
 
-        const remaining_move = if (move < 0)
-            move + dist_to_0
-        else
-            move - dist_to_0;
-
-           
-
-        if (@abs(move) > dist_to_0) {
-            pass += 1 + (@abs(move) / 100); 
-        }
-        while (pos < 0) {
-            pos += 100;
-        }
-        while (pos >= 100) {
-            pos -= 100;
-        }
-        if (pos == 0) {
-            pass += 1;
-        }
-        try stdout.print("{d}\n", .{pos});
-        try stdout.flush();
+        try w.print("{d}\n", .{pos});
+        try w.flush();
     }
 
-    try stdout.print("Pass: {d}\n", .{pass});
-    try stdout.flush();
+    try w.print("Pass: {d}\n", .{pass});
+    try w.flush();
+
+    return pass;
+}
+
+test {
+    const test_input = 
+        \\L68
+        \\L30
+        \\R48
+        \\L5
+        \\R60
+        \\L55
+        \\L1
+        \\L99
+        \\R14
+        \\L82
+    ;
+
+    var test_reader: Reader = .fixed(test_input[0..]);
+    const r = &test_reader;
+    var the_void: [1024]u8 = undefined;
+    var test_writer: Writer.Discarding = .init(&the_void);
+    const w = &test_writer.writer;
+    
+    try std.testing.expectEqual(6, try getPass(r, w));
+}
+
+pub fn main() !void {
+    _ = try getPass(stdin, stdout);
 }
 
 var inbuf: [1024]u8 = undefined;
