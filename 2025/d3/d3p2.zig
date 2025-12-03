84764a6807edad0d9b6570f87951296a8af82388
spicydll <mason.schmidgall1@gmail.com>
2025-12-03 14:42:10 -0600

    finish d3p2

--- 2025/d3/d3p2.zig
+++ 2025/d3/d3p2.zig
@@ -5,7 +5,7 @@ const Allocator = std.mem.Allocator;
 
 fn parseBank(alloc: Allocator, r: *Reader) !?[]u8 {
     const line = try r.takeDelimiter('\n') orelse return null;
-    if (line.len < 2) {
+    if (line.len < 12) {
         return error.BankTooSmall;
     }
     for (line) |char| {
@@ -17,29 +17,46 @@ fn parseBank(alloc: Allocator, r: *Reader) !?[]u8 {
 }
 
 /// Calculates joltage from bank
-/// Asserts that bank is at least length 2 and consists of only ascii digits
+/// Asserts that bank is at least length 12 and consists of only ascii digits
 fn largestJoltage(bank: []const u8) usize {
-    std.debug.assert(bank.len >= 2);
+    const num_batts: comptime_int = 12;
+    std.debug.assert(bank.len >= num_batts);
     // find largest possible first number
-    var first_large_idx: usize = 0;
-    for (bank[1..bank.len - 1], 1..) |first, idx| {
-        if (first > bank[first_large_idx]) {
-            first_large_idx = idx;
+    var large_idxs: [num_batts]usize = blk: {
+        var idxs: [num_batts]usize = undefined;
+        for (&idxs, 0..) |*idx_ptr, idx_val| {
+            idx_ptr.* = idx_val;
+        }
+        break :blk idxs;
+    };
+    for (&large_idxs, 0..) |*cur_large, cur_idx| {
+        cur_large.* = if (cur_idx == 0) 0 else large_idxs[cur_idx - 1] + 1;
+        const start_idx = cur_large.* + 1;
+        const stop_idx = (bank.len - (num_batts - cur_idx)) + 1;
+        for (bank[start_idx..stop_idx], start_idx..) |new_val, new_idx| {
+            if (new_val > bank[cur_large.*]) {
+                cur_large.* = new_idx;
+            }
         }
     }
-
-    // find largest possible second number
-    var second_large_idx = first_large_idx + 1;
-    for (bank[first_large_idx + 1..], first_large_idx + 1..) |second, idx| {
-        if (second > bank[second_large_idx]) {
-            second_large_idx = idx;
+    const num_str: [num_batts]u8 = blk: {
+        var str: [num_batts]u8 = undefined;
+        for (&str, 0..) |*char, idx| {
+            const batt = bank[large_idxs[idx]];
+            std.debug.assert(batt >= '0' and batt <= '9');
+            char.* = batt;
         }
-    }
+        break :blk str;
+    };
 
-    const num_str: [2]u8 = .{ bank[first_large_idx], bank[second_large_idx] };
-    std.debug.assert(num_str[0] >= '0' and num_str[0] <= '9');
-    std.debug.assert(num_str[1] >= '0' and num_str[1] <= '9');
     return std.fmt.parseInt(usize, num_str[0..], 10) catch unreachable;
+}
+
+test largestJoltage {
+    try std.testing.expectEqual(987654321111, largestJoltage("987654321111111"));
+    try std.testing.expectEqual(811111111119, largestJoltage("811111111111119"));
+    try std.testing.expectEqual(434234234278, largestJoltage("234234234234278"));
+    try std.testing.expectEqual(888911112111, largestJoltage("818181911112111"));
 }
 
 pub fn main() !void {
