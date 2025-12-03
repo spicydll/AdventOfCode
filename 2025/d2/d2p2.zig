52d9a981707c0a70a2c22a47cf5ad6e927e1d62f
spicydll <mason.schmidgall1@gmail.com>
2025-12-03 12:58:50 -0600

    finish d2p2.zig

--- 2025/d2/d2p2.zig
+++ 2025/d2/d2p2.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Reader = std.io.Reader;
 const Writer = std.io.Writer;
+const Allocator = std.mem.Allocator;
 
 const Range = struct {
     begin: usize,
@@ -18,62 +19,52 @@ const Range = struct {
     }
 };
 
-const SplitNumber = struct {
-    high: usize,
-    low: usize,
-
-    pub fn split(number: usize) ?SplitNumber {
-        const num_size = @as(usize, @intFromFloat(std.math.log10(@as(f64, @floatFromInt(number))))) + 1;
-        if (num_size & 1 == 1) {
-            return null;
-        }
-        const half_size = num_size / 2;
-        const half_to_10 = std.math.pow(usize, 10, half_size);
-        const top_part = number / half_to_10;
-        const bottom_part = number % half_to_10;
-
-        return .{
-            .high = top_part,
-            .low = bottom_part,
-        };
-    }
-};
-
-test SplitNumber {
-    const TestCase = struct {
-        expect: ?SplitNumber,
-        input: usize,
-    };
-    const test_cases = [_]TestCase{
-        .{ 
-            .expect = .{ .high = 1234, .low = 5678 },
-            .input = 12345678,
-        },
-        .{ 
-            .expect = .{ .high = 5678, .low = 5678 },
-            .input = 56785678,
-        },
-        .{ 
-            .expect = null,
-            .input = 5678567,
-        },
-    };
-
-    for (test_cases) |case| {
-        try std.testing.expectEqualDeep(case.expect, SplitNumber.split(case.input));
-    }
+fn toStr(alloc: Allocator, number: usize) ![]u8 {
+    return try std.fmt.allocPrint(alloc, "{d}", .{number}); 
 }
+
+fn isRepeating(string: []const u8) bool {
+    outer: for (2..string.len + 1) |num_splits| {
+        if (string.len % num_splits != 0) {
+            continue;
+        }
+        const size = string.len / num_splits;
+        const check = string[0..size];
+        var idx = size;
+        while (idx < string.len) : (idx += size) {
+            const compare = string[idx..idx + size];
+            if (!std.mem.eql(u8, check, compare)) {
+                continue :outer;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+test isRepeating {
+    try std.testing.expect(isRepeating("12341234"));
+    try std.testing.expect(isRepeating("11111111"));
+    try std.testing.expect(!isRepeating("11112111"));
+    try std.testing.expect(isRepeating("1111111"));
+    try std.testing.expect(!isRepeating("1211111"));
+}
+
 pub fn main() !void {
+    const page_alloc = std.heap.page_allocator;
+    var arena: std.heap.ArenaAllocator = .init(page_alloc);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    
     var invalid_id_sum: usize = 0;
     while (try Range.parseRange(stdin)) |range| {
         var cur_id: usize = range.begin;
         while (cur_id <= range.end) : (cur_id += 1) {
-            const maybe_split: ?SplitNumber = .split(cur_id);
-            if (maybe_split) |split| {
-                if (split.high == split.low) {
-                    invalid_id_sum += cur_id;
-                    std.debug.print("{d} ({d} == {d})\n", .{cur_id, split.high, split.low});
-                }
+            const id_str = try toStr(alloc, cur_id);
+            defer alloc.free(id_str);
+
+            if (isRepeating(id_str)) {
+                invalid_id_sum += cur_id;
             }
         }
     }
